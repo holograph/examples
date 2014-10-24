ffcf4735b842b1273bd714522d5d9c92375fe029
Tomer Gabel <tomerga@wix.com>
2014-10-24 12:20:17 +0200

    Extended predicate model for negation

--- scala-dsl-guide/src/main/scala/com/tomergabel/examples/dsl/Final.scala
+++ scala-dsl-guide/src/main/scala/com/tomergabel/examples/dsl/Final.scala
@@ -10,6 +10,7 @@ object Final extends App {
   def empty[T <: GenTraversableOnce[_]] = new CompoundPredicate[T] {
     def test: T => Boolean = _.isEmpty
     def failure = "is not empty"
+    def failureNeg = "is empty"
   }
 
   implicit class EntryPoint[T](data: T) {
@@ -29,11 +30,13 @@ object Final extends App {
   implicit class BooleanPredicate(b: Boolean) extends CompoundPredicate[Boolean] {
     def test: Boolean => Boolean = _ == b
     def failure = s"is not $b"
+    def failureNeg = s"is $b"
   }
 
   def equalTo[T](rhs: T) = new CompoundPredicate[T] {
     def test: T => Boolean = _ == rhs
-    def failure = "is not empty"
+    def failure = s"is not equal to $rhs"
+    def failureNeg = s"is equal to $rhs"
   }
 
   Nil shouldBe empty
@@ -46,25 +49,28 @@ object Final extends App {
   def startWith(prefix: String) = new ModalPredicate[String] {
     def test: String => Boolean = _ startsWith prefix
     def failure = s"does not start with $prefix"
+    def failureNeg = s"starts with $prefix"
   }
   def endWith(suffix: String) = new ModalPredicate[String] {
     def test: String => Boolean = _ endsWith suffix
     def failure = s"does not end with $suffix"
+    def failureNeg = s"ends with $suffix"
   }
   def contain[T <: GenTraversableOnce[E], E](elem: E) =
     new ModalPredicate[T] {
       def test: T => Boolean = _.exists(_ == elem)
       def failure = s"does not contain element $elem"
+      def failureNeg = s"contains element $elem"
     }
   def matchRegex(pattern:String) = new ModalPredicate[String] {
     def test: String => Boolean = _ matches pattern
     def failure = s"does not match pattern $pattern"
+    def failureNeg = s"matches pattern $pattern"
   }
 
   "Scala.IO" should startWith("Scala")
   "Scala.IO" should endWith("IO")
   List(1, 2, 3) should contain(2)
-//  List(1, 2, 3) shouldBe contain(2)
   "Scala.IO" should matchRegex("Sc.*")
 
 
--- scala-dsl-guide/src/main/scala/com/tomergabel/examples/dsl/Predicate.scala
+++ scala-dsl-guide/src/main/scala/com/tomergabel/examples/dsl/Predicate.scala
@@ -4,9 +4,26 @@ package com.tomergabel.examples.dsl
  * Created by tomer on 10/24/14.
  */
 sealed trait Predicate[-T] {
+  type Self[-T] <: Predicate[T]
   def test: T => Boolean
   def failure: String
+  def failureNeg: String
+  def negate: Self[T]
 }
 
-trait ModalPredicate[-T] extends Predicate[T]
-trait CompoundPredicate[-T] extends Predicate[T]
+trait ModalPredicate[-T] extends Predicate[T] { self =>
+  type Self[-T] = ModalPredicate[T]
+  def negate = new ModalPredicate[T] {
+    def test = self.test andThen { !_ }
+    def failure = self.failureNeg
+    def failureNeg = self.failure
+  }
+}
+trait CompoundPredicate[-T] extends Predicate[T] { self =>
+  type Self[-T] = CompoundPredicate[T]
+  def negate = new CompoundPredicate[T] {
+    def test = self.test andThen { !_ }
+    def failure = self.failureNeg
+    def failureNeg = self.failure
+  }
+}
