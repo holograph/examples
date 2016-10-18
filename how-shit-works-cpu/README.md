c6796f8890c8c5c3a5fba739e8be17aa2c37af9c
Tomer Gabel <tomer@tomergabel.com>
2016-10-18 18:53:57 +0300

    Added initial samples for the "How Shit Works: CPU" talk

---
+++ how-shit-works-cpu/README.md
@@ -0,0 +1,2 @@
+Exmaple code for the "How Shit Works: The CPU" talk from BuildStuff 2016 Lithuania.
+
---
+++ how-shit-works-cpu/src/main/java/com/tomergabel/examples/cpu/branching/Presorted.java
@@ -0,0 +1,34 @@
+// Based off of http://stackoverflow.com/questions/21432400/is-branch-prediction-not-working
+
+package com.tomergabel.examples.cpu.branching;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@BenchmarkMode(Mode.AverageTime)
+@Warmup(iterations = 2, time = 1)
+@Measurement(iterations = 3, time = 1)
+@State(Scope.Thread)
+@Fork(2)
+public class Presorted {
+    private final byte[] data = new byte[Configuration.DATA_SIZE];
+
+    @Setup
+    public void setup() {
+        new Random(Configuration.RANDOM_SEED).nextBytes(data);
+        Arrays.sort(data);
+    }
+
+    @Benchmark
+    public long sum() {
+        long sum = 0;
+        for (int c = 0; c < Configuration.DATA_SIZE; c++)
+            if (data[c] >= 0)
+                sum += data[c];
+        return sum;
+    }
+}
